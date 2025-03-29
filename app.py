3d2411721282f31d5bb746bbcc74508583ddbfc9
Labhansh Timande <labhanshtimande4@gmail.com>
2025-03-29 14:02:10 -0500

    package info

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  # Import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('login.html')  # Render the login.html template
 
 if __name__ == '__main__':
     app.run(debug=True)
